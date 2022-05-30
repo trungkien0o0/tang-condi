2c1748d8a46a8acf156bc0c82aa83814d4229ce3
trungkien0o0 <106523016+trungkien0o0@users.noreply.github.com>
2022-05-30 19:27:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,14 @@ const CONFIG = {
     introDesc: `Cái web đáng ghét+ đáng bị loại bỏ này đã tổn của t cả buổi chiều,mong G chân thành trả lời`,
     btnIntro: 'Đồng ý',
     title: 'G ngoo phải khum :>',
-    desc: 'K trả lời là có. ',
+    desc: 'K trả lời là có.',
     btnYes: 'Đúng gòii :<',
-    btnNo: 'Không'
+    btnNo: 'Không',
     question:'Thương G quá bị vậy lâu chưa =)))',
     btnReply: 'Gửi',
     reply: 'Bẩm sinh gòiii :<',
-    mess: ' Biết ngay mà =))',
-    messDesc: 'Liệu G có thể xuống ngồi cùng t để giải cứu t khỏi bọn cgai k :D'
-    btnAccept: 'Okiiiii ',
+    mess: 'Biết ngay mà =))',
+    messDesc: 'Liệu G có thể nào bớt đáng iu lại k :D',
+    btnAccept: '...',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
-```
